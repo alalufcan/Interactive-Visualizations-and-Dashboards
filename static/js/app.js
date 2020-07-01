006654bf99bc74e0456aa8bbe9601346c0cee583
alalufcan <alalufcan@gmail.com>
2020-07-01 16:52:37 -0400

    cc

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,20 @@
-  d3.json("data/samples.json").then((data) => {
-    // To be sure that we get the data
-    console.log(data);
+d3.json("data/samples.json").then((data) => {
+  // To be sure that we get the data
+  console.log(data);
 
-    var metadata = data.metadata;
-    
-    console.log(metadata);
+  // Assigning values to the variables
+  var metaData = data.metadata;
+  console.log("MetaData:", metaData);
 
-});
+  var names = data.names;
+  console.log("Names:", names);
+
+  var sampleData = data.samples;
+  console.log("SampleData:", sampleData);
+  
+  // Filling the Dropdown menu for Test Subject ID No.
+  var dropDown = d3.select("#selDataset");
+  names.forEach((name) =>{
+      var cell = dropDown.append("option");
+      cell.text(name);
+  });
